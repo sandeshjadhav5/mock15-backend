f1ca1d6997899d02856e75c2d594d572000f0513
sandeshjadhav5 <jadhavs932@gmail.com>
2023-03-07 13:19:29 +0530

    added patch and delete request

--- routes/Jobs.routes.js
+++ routes/Jobs.routes.js
@@ -28,6 +28,33 @@ jobsRouter.post("/add", async (req, res) => {
   }
 });
 
+// E D I T   J O B S
+jobsRouter.patch("/edit/:id", async (req, res) => {
+  const payload = req.body;
+  const id = req.params.id;
+
+  const job = await JobsModel.find({ _id: id });
+  try {
+    await JobsModel.findByIdAndUpdate({ _id: id }, payload);
+    res.send("Updated Successfully");
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: "Something Went Wrong" });
+  }
+});
+
+// D E L E T E  J O B
+jobsRouter.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    await JobsModel.findByIdAndDelete({ _id: id });
+    res.send("Deleted ");
+  } catch (err) {
+    console.log(err);
+    res.send("Error in Deleting");
+  }
+});
+
 module.exports = {
   jobsRouter,
 };
